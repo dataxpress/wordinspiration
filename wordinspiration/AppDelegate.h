9103ec064d1ebc0f370002f8aa26952f22ee361f
Tim Gostony <dataxpress@gmail.com>
2016-04-10 12:03:09 -0700

    Initial Commit

---
+++ wordinspiration/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wordinspiration
+//
+//  Created by Tim Gostony on 4/10/16.
+//  Copyright © 2016 Tim Gostony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ wordinspiration/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wordinspiration
+//
+//  Created by Tim Gostony on 4/10/16.
+//  Copyright © 2016 Tim Gostony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
